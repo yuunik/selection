ce1d9731ddfb59334fa4c89b333137b44a4d9896
yuunik <yuunik.zhou@gmail.com>
2026-05-08 14:46:15 +0800

    refactor: 代码格式化

--- selection-model/src/main/java/com/yuunik/selection/model/vo/common/ResultCodeEnum.java
+++ selection-model/src/main/java/com/yuunik/selection/model/vo/common/ResultCodeEnum.java
@@ -5,9 +5,9 @@ import lombok.Getter;
 @Getter // 提供获取属性值的getter方法
 public enum ResultCodeEnum {
 
-    SUCCESS(200 , "操作成功") ,
+    SUCCESS(200 , "操作成功"),
     LOGIN_ERROR(201 , "用户名或者密码错误"),
-    VALIDATE_CODE_ERROR(202 , "验证码错误") ,
+    VALIDATE_CODE_ERROR(202 , "验证码错误"),
     USER_NOT_EXISTS(203 , "用户不存在"),
     DATA_ERROR(204, "数据异常"),
     TOKEN_EXPIRED(205, "token失效"),
@@ -18,16 +18,14 @@ public enum ResultCodeEnum {
     NODE_ERROR( 217, "该节点下有子节点，不可以删除"),
     SQL_ERROR( 218, "sql执行错误"),
     STOCK_LESS( 219, "库存不足"),
-    FILE_UPLOAD_ERROR( 220, "文件上传失败"),
+    FILE_UPLOAD_ERROR( 220, "文件上传失败");
 
-    ;
-
-    private Integer code ;      // 业务状态码
-    private String message ;    // 响应消息
+    private Integer code;      // 业务状态码
+    private String message;    // 响应消息
 
     private ResultCodeEnum(Integer code , String message) {
-        this.code = code ;
-        this.message = message ;
+        this.code = code;
+        this.message = message;
     }
 
 }
